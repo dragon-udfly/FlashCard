75d2930a0a0ebe71d50cfbcf065bb1e09e432afa
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-11-24 22:25:22 +0530

    Updated firebaseconfig.tsx file

--- app/login/firebaseconfig.tsx
+++ app/login/firebaseconfig.tsx
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth"; 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,6 +17,11 @@ const firebaseConfig = {
   measurementId: "G-YCHC4GRCWB"
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { auth };
+export default app;
